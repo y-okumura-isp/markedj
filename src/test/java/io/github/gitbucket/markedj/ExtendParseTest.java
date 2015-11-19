7e96b3137ca4b86ca7b4e5dca8e27416d311642b
koda <koda.masaru3@gmail.com>
2015-11-20 05:43:59 +0900

    相対パスで始まるものを、同一ドメインと判定して、 httpから始まる場合はtarget="_blank"を付与して、 相対パスの時はつけないようにした

---
+++ src/test/java/io/github/gitbucket/markedj/ExtendParseTest.java
@@ -0,0 +1,40 @@
+package io.github.gitbucket.markedj;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class ExtendParseTest {
+
+	@Test
+	public void testLinkTarget() throws Exception {
+		Options options = new Options();
+		options.setBreaks(true);
+		
+		String md = loadResourceAsString("link_target.md");
+		String result = Marked.marked(md, options);
+		System.out.println(result);
+		String expect = loadResourceAsString("link_target.html");
+		//assertEquals(expect, result);
+	}
+
+	private String loadResourceAsString(String path) throws IOException {
+		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024 * 8];
+			int length = 0;
+			while ((length = in.read(buf)) != -1) {
+				out.write(buf, 0, length);
+			}
+			return new String(out.toByteArray(), "UTF-8");
+		} finally {
+			in.close();
+		}
+	}
+
+}
